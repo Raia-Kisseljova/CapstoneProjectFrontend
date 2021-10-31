98c07c9c2efef58baff1a0d1ae0608e841e032cb
Raia Kisseljova <84378854+Raissa-Kisseljova@users.noreply.github.com>
2021-10-31 15:46:00 +0000

    feat:hooks and providers for the future use

---
+++ src/hooks/useAnimal.ts
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function useAnimal() {
+  return;
+}
+export { useAnimal };
---
+++ src/hooks/useAnimals.ts
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function useAnimals() {
+  return;
+}
+export { useAnimals };
