6569f9bb69c4673d5e16dbed3486175946349543
Raia Kisseljova <84378854+Raissa-Kisseljova@users.noreply.github.com>
2021-11-01 19:24:49 +0000

    feat: working on login component -connect with backend

--- src/components/pages/LogIn/Login.tsx
+++ src/components/pages/LogIn/Login.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import axios from 'axios'
+import decode from 'jwt-decode'
+import React, { useRef } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
+import { Link, useHistory } from 'react-router-dom'
 import ButtonCustom from '../../shared/Button/ButtonCustom'
 import styles from "./Login.module.css"
 
+
+
+// import { Link } from
 export default function Login() {
+  const history = useHistory()
+  const emailRef = useRef<HTMLInputElement>(null)
+  const passwordRef = useRef<HTMLInputElement>(null)
+
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const emailInput = emailRef.current as HTMLInputElement;
+    const passwordInput = passwordRef.current as HTMLInputElement;
+
+    const credentials = {
+      email: emailInput.value,// ''
+      password: passwordInput.value, // ''
+    }
+
+    try {
+
+      const res = await axios.post("http://localhost:3001/login", credentials);
+      console.log(res)
+      const decodedToken = decode(res.data.accessToken) as { nickname: string }
+      console.log(decodedToken)
+      localStorage.setItem('accessToken', res.data.accessToken);
+      history.push(`users/${decodedToken.nickname}`)
+
+    } catch (err) {
+      if (axios.isAxiosError(err) && err.response) {
+        const { status, data } = err.response;
+        console.log({ status, data });
+      }
+    }
+
+  }
+
   return (
     <Container fluid="md" className={styles.body}>
       <Row >
@@ -15,14 +54,18 @@ export default function Login() {
           </div>
         </Col>
         <Col >
-          <div className={styles.form}>
-            <h4>Login to your account.</h4>
-            <input type="text" placeholder="email" />
-            <input type="password" placeholder="password" />
-            <ButtonCustom color='blue' className='mr-5'>Login</ButtonCustom>
-            <ButtonCustom color='pink' className='mb-3'>Signup</ButtonCustom>
+          <form onSubmit={onSubmit}>
+            <div className={styles.form}>
+              <h4>Login to your account.</h4>
+              <input type="text" placeholder="email" ref={emailRef} />
+              <input type="password" placeholder="password" ref={passwordRef} />
+              <ButtonCustom color='blue' className='mr-5'>Login</ButtonCustom>
 
-          </div>
+              <Link to={"/signup"}>
+                <ButtonCustom color='pink' className='mb-3' >Signup</ButtonCustom>
+              </Link>
+            </div>
+          </form>
         </Col>
 
 
