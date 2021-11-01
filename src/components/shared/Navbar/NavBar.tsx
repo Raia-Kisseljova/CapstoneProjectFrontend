cb4de45ce46ab8fb4ccf78c770a05ca5924b0b9b
Raia Kisseljova <84378854+Raissa-Kisseljova@users.noreply.github.com>
2021-11-01 19:27:03 +0000

    refactor: replace bootstrap Nav.Link with Link to

--- src/components/shared/Navbar/NavBar.tsx
+++ src/components/shared/Navbar/NavBar.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
-import { Navbar, Container, Nav } from 'react-bootstrap'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 export default function NavBar() {
   return (
     <Navbar className="navbar">
       <Container>
-        <Navbar.Brand href="#home"><img src="assets/images/Logo.png" alt="" className="logo" /></Navbar.Brand>
+        <Navbar.Brand href="#home"><img src="/assets/images/Logo.png" alt="" className="logo" /></Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home" className="bold">Home</Nav.Link>
-          <Nav.Link href="#home" className="bold">Adopt</Nav.Link>
-          <Nav.Link href="#features" className="bold">Login</Nav.Link>
-          <Nav.Link href="#pricing" className="bold">Profile</Nav.Link>
+          <Link to={"/"} className="nav-link bold">
+            Home
+          </Link>
+          <Link to="/users/:nickname" className="nav-link bold">
+            Profile
+          </Link>
+          <Link to={"/login"} className="nav-link bold">Login</Link>
+
         </Nav>
       </Container>
     </Navbar>
