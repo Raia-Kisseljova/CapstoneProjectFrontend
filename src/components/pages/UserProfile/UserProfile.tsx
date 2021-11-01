4fe0e67826c0a04c64b55ba37a183fd6fb060355
Raia Kisseljova <84378854+Raissa-Kisseljova@users.noreply.github.com>
2021-11-01 19:27:42 +0000

    feat: UserProfile component part 1

--- src/components/pages/UserProfile/UserProfile.tsx
+++ src/components/pages/UserProfile/UserProfile.tsx
@@ -1,10 +1,38 @@
-import { userInfo } from 'os'
-import React from 'react'
 
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import styles from "./UserProfile.module.css"
 export default function UserProfile() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Container className={styles.body}>
+      <Container className={styles.block}>
+        <Row className="">
+          <Col>
+            <img src="/assets/images/adoptme.jpg" alt="" width="60%" />
+          </Col>
+          <Col>
+            <div>
+              <h6>Puppy</h6>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+
+            <div className="mt-5">
+              <h6>Age : 2 years.</h6>
+              <h6>Location : Bristol.</h6>
+              <h6>Occupation : N/A</h6>
+              <h6>Hobby : Going for a walk.</h6>
+            </div>
+          </Col>
+
+
+        </Row>
+
+
+      </Container>
+
+
+    </Container>
   )
 }
