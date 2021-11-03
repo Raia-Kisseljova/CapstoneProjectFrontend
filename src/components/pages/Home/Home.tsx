e5985aea304dd334739677561df9c3ace673ce49
Raia Kisseljova <84378854+Raissa-Kisseljova@users.noreply.github.com>
2021-11-03 19:14:02 +0000

    feat: add animation to the homepage

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -6,66 +6,95 @@ import styles from './Home.module.css';
 export default function Home() {
   return (
     // BLOCK 1
-    <div>
-      <Container className={styles['block-1']}>
-        <Row className="mx-3">
+    <>
 
-          <Col className={styles["header-block-1"]} >
+      <div>
 
-            <p>Find your new <span>family</span> member.</p>
-
-          </Col>
-          <Col >
-            <img src="/assets/images/Shelter.png" alt="" className={styles["shelter-img"]} />
-          </Col>
-
-        </Row>
-      </Container>
-
-      <Container>
-        <Row className="mx-3">
-          <Col className={styles['section-2']}>
-            <p>Be a proud foster parent. Adopt your new family member today.</p>
-            <button className='post-btn mr-5'>Post</button>
-            <button className='search-btn'>Search</button>
-          </Col>
-        </Row>
-
-      </Container>
-
-      {/* BLOCK 2 */}
-
-      <Container className={styles['block-2']}>
-        <Row className="mx-3">
-          <Col>
-            <p>Where pets turn out to be a <span>family.</span></p>
-            <p className={styles['sub-title']}>Unconditional love is as close as your nearest shelter.</p>
-            <label>
-              <input type="text" placeholder="City" className={styles['search-field']} />
-              <button className={styles['search-btn-group']}>Search</button>
-            </label>
-          </Col>
-        </Row>
-      </Container>
+        <Container fluid={true} className={styles['block-1']}>
+          <Row className="mx-3">
 
 
+            <Col className={styles["header-block-1"]} >
 
-      {/* BLOCK 3 */}
+              <p>Find your new <span>family</span> member.</p>
 
-      <Container className={styles['block-3']}>
-        <Row className="mx-3">
-          <Col>
-            <p>Find inside <span>succesfull</span> stories.</p>
-            <p className={styles['sub-title']}>
-              Succesfull stories from people who adopted.
-            </p>
-          </Col>
-        </Row>
-        <Row>
-          {/* Space for blogs */}
-        </Row>
-      </Container>
-    </div >
+            </Col>
+            <Col >
+              <div className={styles.background}>
+                <img src="/assets/images/paw.png" alt="" />
+                <img src="/assets/images/paw.png" alt="" />
+                <img src="/assets/images/paw.png" alt="" />
+                <img src="/assets/images/paw.png" alt="" />
+                <img src="/assets/images/paw.png" alt="" />
+                <img src="/assets/images/paw.png" alt="" />
+                <img src="/assets/images/paw.png" alt="" />
+                <img src="/assets/images/paw.png" alt="" />
+                <img src="/assets/images/paw.png" alt="" />
+                <img src="/assets/images/paw.png" alt="" />
+                <img src="/assets/images/paw.png" alt="" />
+                <img src="/assets/images/paw.png" alt="" />
+                <img src="/assets/images/paw.png" alt="" />
+                <img src="/assets/images/paw.png" alt="" />
+                <img src="/assets/images/paw.png" alt="" />
+                <img src="/assets/images/paw.png" alt="" />
+                <img src="/assets/images/paw.png" alt="" />
+                <img src="/assets/images/paw.png" alt="" />
+                <img src="/assets/images/paw.png" alt="" />
+
+              </div>
+              <img src="/assets/images/Shelter.png" alt="" className={styles["shelter-img"]} />
+            </Col>
+
+          </Row>
+        </Container>
+
+        <Container fluid={true}>
+          <Row>
+            <Col className={styles['section-2']}>
+              <p>Be a proud foster parent. Adopt your new family member today.</p>
+              <button className='post-btn mr-5'>Post</button>
+              <button className='search-btn'>Search</button>
+            </Col>
+          </Row>
+
+        </Container>
+
+        {/* BLOCK 2 */}
+
+        <Container fluid={true} className={styles['block-2']}>
+          <Row className="mx-3">
+            <Col>
+              <p>Where pets turn out to be a <span>family.</span></p>
+              <p className={styles['sub-title']}>Unconditional love is as close as your nearest shelter.</p>
+              <label>
+                <input type="text" placeholder="City" className={styles['search-field']} />
+                <button className={styles['search-btn-group']}>Search</button>
+              </label>
+
+              <img src="/assets/images/6.png" alt="" width={"170px"} />
+            </Col>
+          </Row>
+        </Container>
+
+
+
+        {/* BLOCK 3 */}
+
+        <Container fluid={true} className={styles['block-3']}>
+          <Row className="mx-3">
+            <Col>
+              <p>Find inside <span>succesfull</span> stories.</p>
+              <p className={styles['sub-title']}>
+                Succesfull stories from people who adopted.
+              </p>
+            </Col>
+          </Row>
+          <Row>
+            {/* Space for blogs */}
+          </Row>
+        </Container>
+      </div >
+    </>
   )
 }
 
