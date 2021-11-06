2a11122f8a7840fa7ba1f23eeeccbd8ea12f7fe9
Raia Kisseljova <84378854+Raissa-Kisseljova@users.noreply.github.com>
2021-11-06 14:56:59 +0000

    feat: navbar component adjustments

--- src/components/shared/Navbar/NavBar.tsx
+++ src/components/shared/Navbar/NavBar.tsx
@@ -1,23 +1,45 @@
-import React from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { useUser } from 'providers/UserProvider';
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link, useHistory } from 'react-router-dom';
 
 export default function NavBar() {
+  const { user, logout } = useUser();
+  const history = useHistory();
+  // console.log('NavBar rerender');
+  console.log(user);
+
   return (
-    <Navbar className="navbar">
-      <Container>
+    <Navbar className="navbar px-5">
+      <Container fluid={true}>
         <Navbar.Brand href="#home"><img src="/assets/images/Logo.png" alt="" className="logo" /></Navbar.Brand>
         <Nav className="me-auto">
           <Link to={"/"} className="nav-link bold">
             Home
           </Link>
-          <Link to="/users/:nickname" className="nav-link bold">
+
+          {user && (
+            <Link to={`/users/${user._id}`} className="nav-link bold">
+              Profile
+            </Link>
+          )}
+          {/* 
+          <Link to={`/users/:nickname`} className="nav-link bold">
             Profile
-          </Link>
-          <Link to={"/login"} className="nav-link bold">Login</Link>
+          </Link> */}
+
+          {user == null && (
+            <Link to={"/login"} className="nav-link bold">Login</Link>
+          )}
+
+          <button className="logout-btn" onClick={() => {
+            logout();
+            history.push('/login');
+          }}>Logout</button>
+
 
         </Nav>
       </Container>
-    </Navbar>
+    </Navbar >
   )
 }
