286671c411a58fb7c9ae429b675cb3b4a9023e76
Raia Kisseljova <84378854+Raissa-Kisseljova@users.noreply.github.com>
2021-11-11 01:21:15 +0000

    feat: userProfile improvements

--- src/components/pages/UserProfile/UserProfile.tsx
+++ src/components/pages/UserProfile/UserProfile.tsx
@@ -1,22 +1,32 @@
 import { AxiosError } from 'axios';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import Skeleton from 'react-loading-skeleton';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router';
 
 import { axios } from 'api';
 import Loader from 'components/shared/Loaders/Loader';
 import useCurrentUser from 'hooks/useCurrentUser';
-import { TUser } from 'types';
+import { TAnimal, TUser } from 'types';
 
 import styles from './UserProfile.module.css';
 
 export default function UserProfile() {
   const { username } = useParams() as { username: string };
 
-  const userQuery = useQuery<TUser, AxiosError>(['USER_DETAIL', username], () =>
+  const userQuery = useQuery<TUser, AxiosError>(['USERS', username], () =>
     axios.get(`user/by_name/${username}`).then(res => res.data)
+  );
+
+  const favouritesQuery = useQuery<TAnimal[], AxiosError>(
+    ['USERS', username, 'FAVORITES'],
+    () => axios.get(`user/${username}/favourites`).then(res => res.data),
+    {
+      enabled: userQuery.isSuccess,
+    }
   );
 
   if (userQuery.isLoading) {
@@ -65,10 +75,27 @@ export default function UserProfile() {
                 <br />
                 <span>Occupation: {userQuery.data?.occupation}</span>
                 <br />
-                <span>City: </span>
+                <span>City:{userQuery.data?.location} </span>
                 <br />
                 <span>Date of Birth: {userQuery.data?.dateOfBirth}</span>
                 <br />
+              </div>
+
+              <div>
+                Favourites
+                {favouritesQuery.isLoading ? (
+                  <div>
+                    <Skeleton circle count={5} width={50} height={50} />
+                  </div>
+                ) : favouritesQuery.isSuccess ? (
+                  <div>
+                    {favouritesQuery.data.map(animal => {
+                      return <div key={animal._id}>{animal.petName}</div>;
+                    })}
+                  </div>
+                ) : favouritesQuery.isError ? (
+                  <div>{favouritesQuery.error.response?.status}</div>
+                ) : null}
               </div>
             </div>
           </Col>
@@ -77,3 +104,10 @@ export default function UserProfile() {
     </Container>
   );
 }
+
+// <AnimalInfo isOwner={currentUser._id === userQuery.data._id}
+
+// AnimalInfo ({ isOwner, ...ainimalStuff })
+// image.resize('300x300');
+// image.crop('16:9');
+// {isOwner &&  <button></button>}
