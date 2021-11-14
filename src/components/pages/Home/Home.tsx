de5209857115cbb711221df6f2a618e42360a6fe
Raia Kisseljova <84378854+Raissa-Kisseljova@users.noreply.github.com>
2021-11-14 06:43:00 +0000

    feat: restyle homepage component

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -3,16 +3,47 @@ import { Col, Container, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 import Animation from 'components/shared/Animations/Animation';
+import useCurrentUser from 'hooks/useCurrentUser';
+import { Role } from 'types';
 
 import styles from './Home.module.css';
 
 export default function Home() {
+  const { user } = useCurrentUser();
+
   return (
     // BLOCK 1
     <>
       <div>
         <Animation />
-        <Container fluid className={styles['block-1']}>
+
+        <Container fluid={true}>
+          <Row className={styles['block-1-test']}>
+            <img
+              src='/assets/images/Shelter.png'
+              alt=''
+              className={styles['shelter-img']}
+            />
+            <p>
+              Find your new <span>family</span> member.
+            </p>
+
+            <p className={styles['sub-title-test']}>
+              Be a proud foster parent. Adopt your new family member today.
+            </p>
+            <div>
+              <Link to='/search'>
+                <button className='search-btn'>Search</button>
+              </Link>
+              {user && user.role === Role.ORGANISATION && (
+                <Link to='/add-animal'>
+                  <button className='post-btn'>Post</button>
+                </Link>
+              )}
+            </div>
+          </Row>
+        </Container>
+        {/* <Container fluid className={styles['block-1']}>
           <Row className='mx-3'>
             <Col className={styles['header-block-1']}>
               <p>
@@ -27,19 +58,27 @@ export default function Home() {
               />
             </Col>
           </Row>
-        </Container>
+        </Container> */}
 
-        <Container fluid>
+        {/* <Container fluid>
           <Row>
             <Col className={styles['section-2']}>
-              <p>Be a proud foster parent. Adopt your new family member today.</p>
-              <button className='post-btn mr-5'>Post</button>
+              <p>Be a proud foster parent.Adopt your new family member today.</p>
+
+              {user && user.role === Role.ORGANISATION && (
+
+                <Link to="/add-animal">
+                  <button className='post-btn mr-5'>Post</button>
+                </Link>
+              )}
+
+
               <Link to='/search'>
                 <button className='search-btn'>Search</button>
               </Link>
             </Col>
           </Row>
-        </Container>
+        </Container> */}
 
         {/* BLOCK 2 */}
 
