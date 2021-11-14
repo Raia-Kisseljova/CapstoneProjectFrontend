cc2b038c9b6ba0dc07e3eba9a854a088ac16b3ab
Raia Kisseljova <84378854+Raissa-Kisseljova@users.noreply.github.com>
2021-11-14 06:40:37 +0000

    feat: improve userProfile component , remove from favourites

--- src/components/pages/UserProfile/UserProfile.tsx
+++ src/components/pages/UserProfile/UserProfile.tsx
@@ -4,17 +4,18 @@ import 'react-loading-skeleton/dist/skeleton.css';
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import Skeleton from 'react-loading-skeleton';
-import { useQuery } from 'react-query';
+import { QueryClient, useQuery, useQueryClient } from 'react-query';
 import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 
 import { axios } from 'api';
 import Loader from 'components/shared/Loaders/Loader';
-import useCurrentUser from 'hooks/useCurrentUser';
 import { TAnimal, TUser } from 'types';
 
 import styles from './UserProfile.module.css';
 
 export default function UserProfile() {
+  const queryClient = useQueryClient();
   const { username } = useParams() as { username: string };
 
   const userQuery = useQuery<TUser, AxiosError>(['USERS', username], () =>
@@ -38,21 +39,26 @@ export default function UserProfile() {
     return <div>Not Found</div>;
   }
 
+  const remove = async (_id: string) => {
+    await axios.delete(`/user/${userQuery.data?.nickname}/favourites/${_id}`);
+    queryClient.invalidateQueries(['USERS', username, 'FAVORITES']);
+  };
+
   return (
     <Container className={styles.body}>
       <Container className={styles.block}>
-        <img src='/assets/images/4-1.png' className={styles.before} alt='Before' />
+        <img src='/assets/images/15.png' className={styles.before} alt='Before' />
         <img src='/assets/images/5.png' className={styles.after} alt='After' />
-        <Row className=''>
+        <Row className='row-cols-1 row-cols-md-2'>
           <Col>
             <div className={styles['img-part']}>
-              <img src='/assets/images/adoptme.jpg' alt='Adopt me' />
+              <img src='/assets/images/tilly.jpg' alt='Adopt me' />
             </div>
             <div className={styles.favorites}>
               <button>
                 <img src='/assets/images/heart.png' alt='Heart' />
               </button>
-              5
+
               <button>
                 <img src='/assets/images/email.png' alt='Email' />
               </button>
@@ -80,26 +86,33 @@ export default function UserProfile() {
                 <span>Date of Birth: {userQuery.data?.dateOfBirth}</span>
                 <br />
               </div>
-
-              <div>
-                Favourites
-                {favouritesQuery.isLoading ? (
-                  <div>
-                    <Skeleton circle count={5} width={50} height={50} />
-                  </div>
-                ) : favouritesQuery.isSuccess ? (
-                  <div>
-                    {favouritesQuery.data.map(animal => {
-                      return <div key={animal._id}>{animal.petName}</div>;
-                    })}
-                  </div>
-                ) : favouritesQuery.isError ? (
-                  <div>{favouritesQuery.error.response?.status}</div>
-                ) : null}
-              </div>
             </div>
           </Col>
         </Row>
+        <div>
+          <h3 className='mt-5'>Favourites</h3>
+          {favouritesQuery.isLoading ? (
+            <div>
+              <Skeleton circle count={5} width={50} height={50} />
+            </div>
+          ) : favouritesQuery.isSuccess ? (
+            <div>
+              {favouritesQuery.data.map(animal => {
+                return (
+                  <div key={animal._id} className={styles.animals}>
+                    <img src={animal.images[0]} alt='img' width='50px' />
+                    <Link to={`/animal/${animal._id}`} className='ml-3'>
+                      {animal.petName}{' '}
+                    </Link>
+                    <button onClick={() => remove(animal._id)}>&times; </button>
+                  </div>
+                );
+              })}
+            </div>
+          ) : favouritesQuery.isError ? (
+            <div>{favouritesQuery.error.response?.status}</div>
+          ) : null}
+        </div>
       </Container>
     </Container>
   );
